cd8ce2a8e7f779425857fb76f7bc79d1fe84adc2
jri-sp jeremy.richard@sciencespo.fr <jeremy.richard@sciencespo.fr>
2015-12-02 14:33:17 +0100

    fix convert encoding + add level in GELF structure

--- GELF.py
+++ GELF.py
@@ -15,11 +15,12 @@ class GELF:
         try:
             # GELF "Header"
             data['version'] = '1.1'
-            data['timestamp'] = time.time(),
+            data['timestamp'] = time.time()
             data['host'] = gethostname()
+            data['level'] = 6
 
             udp_socket = socket(AF_INET,SOCK_DGRAM)
-            compressed_data = zlib.compress(simplejson.dumps(data))
+            compressed_data = zlib.compress(simplejson.dumps(data).encode('utf-8'))
             udp_socket.sendto(compressed_data,(self.graylog_server,self.graylog_port))
             udp_socket.close()
         except Exception, e:
