8dd5daccf2a229feafed64d8901c493a65ea24d4
jri-sp jeremy.richard@sciencespo.fr <jeremy.richard@sciencespo.fr>
2015-12-04 12:40:16 +0100

    JSON dump bug fix

--- getPoolMessages.py
+++ getPoolMessages.py
@@ -6,18 +6,19 @@ from XenServer import XenServer
 from GELF import GELF
 import traceback
 import xmlrpclib
-import time
+from time import time
 
 try:
     xs_session = XenServer().make_session()
     #last 24h messages
-    messages = xs_session.xenapi.message.get_since(xmlrpclib.DateTime(time.time()-86400))
+    messages = xs_session.xenapi.message.get_since(xmlrpclib.DateTime(time()-86400))
     #Get Pool info to match CPU with this pool
     pool = xs_session.xenapi.pool.get_all()[0]
     pool_uuid = xs_session.xenapi.pool.get_uuid(pool)
 
     for message in messages:
         message_record = xs_session.xenapi.message.get_record(message)
+
         data = {
             'short_message': os.path.basename(__file__).split('.')[0],
             '_pool_uuid': pool_uuid,
@@ -26,7 +27,7 @@ try:
             '_name': message_record['name'],
             '_priority': message_record['priority'],
             '_obj_uuid': message_record['obj_uuid'],
-            '_message_timestamp': message_record['timestamp']
+            '_message_timestamp': str(message_record['timestamp'])
 
         }
 
