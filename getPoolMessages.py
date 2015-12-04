65e0514f65106e48cbbb65acdea9205b472b5b95
jri-sp jeremy.richard@sciencespo.fr <jeremy.richard@sciencespo.fr>
2015-12-04 10:08:59 +0100

    Add script to get last 24h pool messages

---
+++ getPoolMessages.py
@@ -0,0 +1,40 @@
+#!/bin/env python
+
+import os
+import sys
+from XenServer import XenServer
+from GELF import GELF
+import traceback
+import xmlrpclib
+import time
+
+from pprint import pprint
+
+try:
+    xs_session = XenServer().make_session()
+    #last 24h messages
+    messages = xs_session.xenapi.message.get_since(xmlrpclib.DateTime(time.time()-86400))
+    #Get Pool info to match CPU with this pool
+    pool = xs_session.xenapi.pool.get_all()[0]
+    pool_uuid = xs_session.xenapi.pool.get_uuid(pool)
+
+    for message in messages:
+        message_record = xs_session.xenapi.message.get_record(message)
+        data = {
+            'short_message': os.path.basename(__file__).split('.')[0],
+            '_pool_uuid': pool_uuid,
+            '_body': message_record['body'],
+            '_cls': message_record['cls'],
+            '_name': message_record['name'],
+            '_priority': message_record['priority'],
+            '_obj_uuid': message_record['obj_uuid'],
+            '_message_timestamp': message_record['timestamp']
+
+        }
+
+        GELF().log_udp(data)
+
+except:
+    sys.stderr.write('ERROR: %s\n' % traceback.format_exc())
+
+
